6db4dfcc19409e7ab48ffd7ea5dd7e5dd162158e
DaniSwi <daniel.cornejo.r@mail.pucv.cl>
2026-04-05 19:01:04 -0400

    Update mi_controlador.py
    Se agregó funcionalidades necesarias para el laboratorio

--- my_project/controllers/mi_controlador/mi_controlador.py
+++ my_project/controllers/mi_controlador/mi_controlador.py
@@ -1,6 +1,8 @@
-from controller import Robot
+from controller import Robot, Keyboard
 
 robot = Robot()
+teclado = Keyboard()
+teclado.enable(int(robot.getBasicTimeStep()))
 
 timestep = int(robot.getBasicTimeStep())
 
@@ -12,13 +14,47 @@ right_motor = robot.getDevice('right wheel motor')
 left_motor.setPosition(float('inf'))
 right_motor.setPosition(float('inf'))
 
-#movimiento recto primero
+#velocidad base
+MAX_VEL = 6.28
+
+"""#movimiento recto primero
 vl = 3.0
 vr = -3.0
 
 left_motor.setVelocity(vl)
-right_motor.setVelocity(vr)
+right_motor.setVelocity(vr) """
+
+"""
+nota del funcionamiento del código:
+se tiene que mantener la tecla para que el robot
+haga el movimiento, por ejemplo
+para que vaya recto hay que 
+mantener la tecla J.
+"""
 
 while robot.step(timestep) != -1:
+    tecla = teclado.getKey()
+    vl = 0.0
+    vr = 0.0
+    
+    #si aprieta J va recto
+    if tecla == ord("J"):
+        vl = 3.0
+        vr = 3.0
+    #si aprieta L es una curva a la izquierda
+    elif tecla == ord("L"):
+        vl = 1.0
+        vr = 4.0
+    #si aprieta R es una curva a la derecha
+    elif tecla == ord("R"):
+        vl = 4.0
+        vr = 1.0
+    elif tecla == ord("O"):
+        vl = 3.0
+        vr = -3.0
+        
+    left_motor.setVelocity(vl)
+    right_motor.setVelocity(vr)
+    
     print("Velocidad derecha = ", vr)
     print("Velocidad izquierda =", vl)
