59827b603cd3f0e123cb7b3974b1f090bb8d9cfa
DaniSwi <daniel.cornejo.r@mail.pucv.cl>
2026-04-06 21:32:16 -0400

    Update mi_controlador.py
    Integrados las ecuaciones de movimiento con velocidad angular y velocidad lineal

--- my_project/controllers/mi_controlador/mi_controlador.py
+++ my_project/controllers/mi_controlador/mi_controlador.py
@@ -15,7 +15,7 @@ left_motor.setPosition(float('inf'))
 right_motor.setPosition(float('inf'))
 
 #velocidad base
-MAX_VEL = 6.28
+#MAX_VEL = 6.28
 
 """#movimiento recto primero
 vl = 3.0
@@ -32,29 +32,37 @@ para que vaya recto hay que
 mantener la tecla J.
 """
 
+L = 0.052 #distancia entre las ruedas del e-puck en metros
+
 while robot.step(timestep) != -1:
     tecla = teclado.getKey()
-    vl = 0.0
-    vr = 0.0
+    v = 0.0
+    w = 0.0
     
     #si aprieta J va recto
     if tecla == ord("J"):
-        vl = 3.0
-        vr = 3.0
+        v = 0.1
+        w = 0.0
     #si aprieta L es una curva a la izquierda
     elif tecla == ord("L"):
-        vl = 1.0
-        vr = 4.0
+        v = 0.05
+        w = 0.5
     #si aprieta R es una curva a la derecha
     elif tecla == ord("R"):
-        vl = 4.0
-        vr = 1.0
+        v = 0.05 
+        w = -0.5
     elif tecla == ord("O"):
-        vl = 3.0
-        vr = -3.0
+        v = 0.0
+        w = 2.0
         
-    left_motor.setVelocity(vl)
-    right_motor.setVelocity(vr)
+    vr = v + (w * L) / 2
+    vl = v - (w * L) / 2
+    
+    #conversion de m/s a rad/s+
+    r = 0.0205
+        
+    left_motor.setVelocity(vl/r)
+    right_motor.setVelocity(vr/r)
     
     print("Velocidad derecha = ", vr)
     print("Velocidad izquierda =", vl)
